2e2b8bf047ba05f158ffc39c9e993582ccf127ca
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-04-24 18:03:01 +0100

    14:03

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -1,7 +1,7 @@
-from brownie import network
+from brownie import network, TokenFarm, exceptions
+import pytest
 from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account, get_contract
 from scripts.deploy import deploy_token_farm_and_dapp_token
-import pytest
 
 def test_set_price_feed_contract():
     # arrange
@@ -13,7 +13,7 @@ def test_set_price_feed_contract():
     # act
     price_feed_address = get_contract("eth_usd_price_feed")
     token_farm.setPriceFeedContract(
-        dapp_token.address, 
+        dapp_token.address,
         price_feed_address,
         {"from": account}
     )
@@ -21,7 +21,32 @@ def test_set_price_feed_contract():
     assert token_farm.tokenPriceFeedMapping(dapp_token.address) == price_feed_address
     with pytest.raises(exceptions.VirtualMachineError):
         token_farm.setPriceFeedContract(
-            dapp_token.address, 
+            dapp_token.address,
             price_feed_address,
             {"from": non_owner}
         )
+
+def test_stake_tokens(amount_staked):
+    # arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # act
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account})
+    # assert
+    assert (
+        token_farm.stakingBalance(dapp_token.address, account.address) == amount_staked
+    )
+    assert token_farm.uniqueTokensStaked(account.address) == 1
+    assert token_farm.stakers(0) == account.address
+    return token_farm, dapp_token
+
+def test_issue_tokens(amount_staked):
+    # arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+    account = get_account()
+    token_farm, dapp_token = test_stake_tokens(amount_staked)
+    starting_balance = dapp_token.balanceOf(account.address)
